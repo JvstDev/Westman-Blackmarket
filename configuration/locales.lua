30f72d93682f7bf9ea360e3f4635bbefd4b2e3d0
JvstDev <148901624+JvstDev@users.noreply.github.com>
2024-09-16 18:22:56 +0200

    Delete configuration directory

--- configuration/locales.lua
+++
@@ -1,7 +0,0 @@
-Locales = {
-    open_blackmarket = "[E] - Open Blackmarket",
-    login_failed = "Your password or username is wrong",
-    you_bought = "You bought",
-    not_enough_money = "You don't have enough money. Your current balance is",
-    cant_carry = "You don't have enough space in your inventory"
-}
