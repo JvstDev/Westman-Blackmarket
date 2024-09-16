8bfa3a823bd84a079f2338a98e80626fa5eda326
JvstDev <148901624+JvstDev@users.noreply.github.com>
2024-09-16 18:24:57 +0200

    Add files via upload

---
+++ en.lua
@@ -0,0 +1,8 @@
+Locales["en"] = { 
+    open_blackmarket = "[E] - Open Blackmarket",
+    login_failed = "Your password or username is wrong",
+    you_bought = "You bought x%s %s",
+    not_enough_money = "You don't have enough money. Your current balance is %s$",
+    cant_carry = "You don't have enough space in your inventory",
+    keybind = "Open keybind"
+}
