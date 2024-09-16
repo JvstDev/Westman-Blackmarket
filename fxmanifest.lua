1d5b700c14d9d62fafbbf7bdb1e2e806c8b48a59
JvstDev <148901624+JvstDev@users.noreply.github.com>
2024-09-16 18:24:05 +0200

    Update fxmanifest.lua
    - Added esx imports
    - Added esx locales

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,23 +1,26 @@
 fx_version "cerulean"
-
 lua54 "yes"
-
 game "gta5"
 
-shared_script {
-  "configuration/locales.lua",
-  "configuration/config.lua"
-} 
+shared_scripts {
+  'config.lua',
+  '@es_extended/imports.lua',
+  '@es_extended/locale.lua',
+  'locales/*.lua'
+}
 
-server_script "server/main.lua"
+server_scripts {
+  "server/main.lua"
+}
 
-client_script "client/main.lua"
+client_scripts {
+  "client/main.lua"
+}
+
+ui_page "html/index.html"
 
 files {
   "html/index.html",
   "html/assets/*.js",
   "html/assets/*.css"
 }
-
-ui_page "html/index.html"
-                  
